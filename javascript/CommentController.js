af68f8c90b52a894b56f5cffaec419cf8f1e3fbb
HenriqueCA <henrique.arriel@ccc.ufcg.edu.br>
2019-06-29 20:10:18 -0300

    author

--- javascript/CommentController.js
+++ javascript/CommentController.js
@@ -5,6 +5,7 @@ async function add_new_comment(subject_id, text) {
     if (text.trim() == "") {
         alert("Comentário vazio");
     } else {
+        alert("CHANGE AUTHOR");
         await post_request(endpoints.subject() + "/" + subject_id + "/comment", { 'author': 'henrique@gmail.com', 'msg': text });
         document.location.reload();
     }
@@ -14,6 +15,7 @@ async function reply_comment(subject_id, text, id) {
     if (text.trim() == "") {
         alert("Comentário vazio");
     } else {
+        alert("CHANGE AUTHOR");
         await post_request(endpoints.subject() + "/" + subject_id + "/comment" + "/" + id, { 'author': 'henrique2@gmail.com', 'msg': text });
         document.location.reload();
     }
